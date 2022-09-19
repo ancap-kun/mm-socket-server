40ae811e01377d94f599e6abc2e61420c287c7da
PukPukov <vovanchikputin@vk.com>
2022-09-19 07:22:21 +0500

    initial

---
+++ src/main/java/ru/ancap/mm/socket/server/SocketServer.java
@@ -0,0 +1,14 @@
+//- Copyright ©2009 Micah Martin.  All Rights Reserved
+//- MMSocketServer and all included source files are distributed under terms of the GNU LGPL.
+
+// Copyright (C) 2009 Micah Martin, Inc. All rights reserved.
+// Released under the terms of the LGNU Lesser General Public License.
+
+package ru.ancap.mm.socket.server;
+
+import java.net.Socket;
+
+public interface SocketServer
+{
+	public void serve(Socket s);
+}
